14accadf2a77473478e77c1784400c2d0ad927ea
Travers Rhodes <tsr42@cornell.edu>
2023-02-04 19:20:04 -0500

    new computer

--- env.sh
+++ env.sh
@@ -5,7 +5,7 @@
 # so that that computer knows how to source relevant ROS definitions and where to find roscore.
 
 # set the ROS_MASTER_URI to my laptop machine (no reason to run rosmaster on the realtime computer)
-export ROS_MASTER_URI="http://helvellyn:11311"
+export ROS_MASTER_URI="http://blau:11311"
 # source the kortex_ros_control_ws workspace (the downloaded and built catkin workspace on the control computer)
 source ~/gravity_comp_ws/devel/setup.bash
 exec "$@"
